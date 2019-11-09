d02d85af2d9b067c05dc2d71304c15ba7cb55efb
josdem <joseluis.delacruz@gmail.com>
2019-11-09 08:30:34 -0500

    Adding configuration xml encoders

--- src/main/java/com/jos/dem/spring/webflux/jackson/xml/controller/PersonController.java
+++ src/main/java/com/jos/dem/spring/webflux/jackson/xml/controller/PersonController.java
@@ -1,20 +1,27 @@
 package com.jos.dem.spring.webflux.jackson.xml.controller;
 
+import com.jos.dem.spring.webflux.jackson.xml.model.Person;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+
+import static org.springframework.http.MediaType.APPLICATION_XML;
+import static org.springframework.http.MediaType.APPLICATION_XML_VALUE;
 
 @RestController
 public class PersonController {
 
   private Logger log = LoggerFactory.getLogger(this.getClass());
 
-  @GetMapping("/")
-  public Mono<String> index(){
+  @GetMapping(value = "/", produces = APPLICATION_XML_VALUE)
+  public Mono<ResponseEntity> index(){
     log.info("Index");
-    return Mono.just("Hello World!");
+    return Mono.just(ResponseEntity.ok()
+            .contentType(APPLICATION_XML)
+    .body(new Person("Jose","Morales","30 Frank Lloyd Ann Arbor MI 48105")));
   }
 
 }
--- src/main/java/com/jos/dem/spring/webflux/jackson/xml/model/Person.java
+++ src/main/java/com/jos/dem/spring/webflux/jackson/xml/model/Person.java
@@ -2,9 +2,14 @@ package com.jos.dem.spring.webflux.jackson.xml.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.xml.bind.annotation.XmlRootElement;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@XmlRootElement
 public class Person {
   private String firstName;
   private String lastName;
