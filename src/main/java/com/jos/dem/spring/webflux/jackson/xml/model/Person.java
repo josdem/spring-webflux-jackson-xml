a99803f521eb7c2ea02f54d45d1c1fc5096b9a1a
josdem <joseluis.delacruz@gmail.com>
2019-11-09 07:35:10 -0500

    Adding Person model

---
+++ src/main/java/com/jos/dem/spring/webflux/jackson/xml/model/Person.java
@@ -0,0 +1,10 @@
+package com.jos.dem.spring.webflux.jackson.xml.model;
+
+
+@Data
+@AllArgsConstructor
+public class Person {
+  private String firstName;
+  private String lastName;
+  private String address;
+}
