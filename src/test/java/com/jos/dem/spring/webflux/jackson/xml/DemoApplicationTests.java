49d0bfabfe3c9c0bb4e697d0e5ce462f10643478
josdem <joseluis.delacruz@gmail.com>
2019-11-09 07:25:59 -0500

    Starting a new Spring Webflux Jackson XML parser project :)

---
+++ src/test/java/com/jos/dem/spring/webflux/jackson/xml/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jos.dem.spring.webflux.jackson.xml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
