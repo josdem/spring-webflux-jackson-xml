26a817ec425be711503656801d7036e0859b74cf
josdem <joseluis.delacruz@gmail.com>
2019-11-09 08:50:19 -0500

    Adding test case

--- src/test/java/com/jos/dem/spring/webflux/jackson/xml/DemoApplicationTests.java
+++ src/test/java/com/jos/dem/spring/webflux/jackson/xml/DemoApplicationTests.java
@@ -1,13 +1,26 @@
 package com.jos.dem.spring.webflux.jackson.xml;
 
+import com.jos.dem.spring.webflux.jackson.xml.model.Person;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static org.springframework.http.MediaType.APPLICATION_XML_VALUE;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
+
+	@Autowired
+	private WebTestClient webClient;
 
 	@Test
 	void contextLoads() {
+		webClient.get().uri("/")
+						.exchange()
+						.expectStatus().isOk()
+						.expectHeader().contentType(APPLICATION_XML_VALUE)
+						.expectBody(Person.class);
 	}
 
 }
