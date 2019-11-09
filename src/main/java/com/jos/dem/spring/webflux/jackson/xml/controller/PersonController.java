cb28fa189b70a54e286fea016c15d3626ac6d63f
josdem <joseluis.delacruz@gmail.com>
2019-11-09 08:40:36 -0500

    Deleting unused code

--- src/main/java/com/jos/dem/spring/webflux/jackson/xml/controller/PersonController.java
+++ src/main/java/com/jos/dem/spring/webflux/jackson/xml/controller/PersonController.java
@@ -3,12 +3,10 @@ package com.jos.dem.spring.webflux.jackson.xml.controller;
 import com.jos.dem.spring.webflux.jackson.xml.model.Person;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
-import static org.springframework.http.MediaType.APPLICATION_XML;
 import static org.springframework.http.MediaType.APPLICATION_XML_VALUE;
 
 @RestController
@@ -17,11 +15,12 @@ public class PersonController {
   private Logger log = LoggerFactory.getLogger(this.getClass());
 
   @GetMapping(value = "/", produces = APPLICATION_XML_VALUE)
-  public Mono<ResponseEntity> index(){
-    log.info("Index");
-    return Mono.just(ResponseEntity.ok()
-            .contentType(APPLICATION_XML)
-    .body(new Person("Jose","Morales","30 Frank Lloyd Ann Arbor MI 48105")));
+  public Mono<Person> index() {
+    log.info("Getting Person");
+    return Mono.just(
+            new Person("Jose",
+                    "Morales",
+                    "30 Frank Lloyd Ann Arbor MI 48105"));
   }
 
 }
