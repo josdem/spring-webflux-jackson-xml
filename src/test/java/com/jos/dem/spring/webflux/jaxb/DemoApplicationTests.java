11fa0a029b1c6465c13d46ad531d03aef859d498
josdem <joseluis.delacruz@gmail.com>
2019-11-16 16:12:41 -0500

    Fixing identation

--- src/test/java/com/jos/dem/spring/webflux/jaxb/DemoApplicationTests.java
+++ src/test/java/com/jos/dem/spring/webflux/jaxb/DemoApplicationTests.java
@@ -2,6 +2,7 @@ package com.jos.dem.spring.webflux.jaxb;
 
 import com.jos.dem.spring.webflux.jaxb.model.Person;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -17,18 +18,19 @@ class DemoApplicationTests {
 	private WebTestClient webClient;
 
 	@Test
-	void shouldGetPerson() {
-		webClient.get().uri("/")
-						.exchange()
-						.expectStatus().isOk()
-						.expectHeader().contentType(APPLICATION_XML_VALUE)
-						.expectBody(Person.class)
-						.value(person -> person.getFirstName(), equalTo("Jose"))
-						.value(person -> person.getLastName(), equalTo("Morales"))
-						.value(person -> person.getAddress(), equalTo("30 Frank Lloyd, Ann Arbor MI 48105"))
-						.value(person -> person.getDevice().getName(), equalTo("Pixel 3"))
-						.value(person -> person.getDevice().getOs(), equalTo("Android"))
-						.value(person -> person.getDevice().getModel(), equalTo("9 Pie"));
+  @DisplayName("Should Get Person")
+  void shouldGetPerson() {
+    webClient.get().uri("/")
+            .exchange()
+            .expectStatus().isOk()
+            .expectHeader().contentType(APPLICATION_XML_VALUE)
+            .expectBody(Person.class)
+            .value(person -> person.getFirstName(), equalTo("Jose"))
+            .value(person -> person.getLastName(), equalTo("Morales"))
+            .value(person -> person.getAddress(), equalTo("30 Frank Lloyd, Ann Arbor MI 48105"))
+            .value(person -> person.getDevice().getName(), equalTo("Pixel 3"))
+            .value(person -> person.getDevice().getOs(), equalTo("Android"))
+            .value(person -> person.getDevice().getModel(), equalTo("9 Pie"));
 	}
 
 }
